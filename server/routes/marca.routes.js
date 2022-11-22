9ff90bfdcef2fb96b38a900ea0c60ee63cef0604
MchavezGP <118851739+MchavezGP@users.noreply.github.com>
2022-11-22 16:26:02 -0500

    Prueba ruta

--- server/routes/marca.routes.js
+++ server/routes/marca.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { createMarca, deleteMarca, getMarcas,getMarca, updateMarca } from '../controllers/marca.controllers.js';
 const router = Router();
-router.post('/marca', createMarca);
+
 router.get('/marca', getMarcas);
+router.post('/marca', createMarca);
 router.get('/marca/:id', getMarca);
 router.delete('/marca/:id', deleteMarca);
 router.put('/marca/:id', updateMarca);
