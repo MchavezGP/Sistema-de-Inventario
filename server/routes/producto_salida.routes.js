2ee3aa80ab87121aeef1f2cf554f972219e9f4c5
MchavezGP <118851739+MchavezGP@users.noreply.github.com>
2022-11-23 00:00:30 -0500

    Changed route productosalida

--- server/routes/producto_salida.routes.js
+++ server/routes/producto_salida.routes.js
@@ -2,6 +2,6 @@ import { Router } from 'express';
 import {  createProductoSalida, getProductosSalidas, deleteProductoSalida } from '../controllers/producto_salida.controllers.js';
 const router = Router();
 router.post('/productosalida', createProductoSalida);
-router.get('/productossalidas', getProductosSalidas);
+router.get('/productosalida', getProductosSalidas);
 router.delete('/productosalida/:id', deleteProductoSalida);
-export default router;
+export default router; 
