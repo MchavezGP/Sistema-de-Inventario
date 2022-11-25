0c860d063b7e24fbfdbefea0ee5644c8ea685763
MchavezGP <118851739+MchavezGP@users.noreply.github.com>
2022-11-24 23:32:44 -0500

    Changed entrada_producto

--- server/routes/entrada_producto.routes.js
+++ server/routes/entrada_producto.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { createEntradaProducto, getEntradasProductos, deleteEntradaProducto} from '../controllers/entrada_producto.controllers.js';
 const router = Router();
-router.post('/entradaproducto', createEntradaProducto);
-router.get('/entradaproducto', getEntradasProductos);
-router.delete('/entradaproducto/:id', deleteEntradaProducto);
+router.post('/entrada_producto', createEntradaProducto);
+router.get('/entrada_producto', getEntradasProductos);
+router.delete('/entrada_producto/:id', deleteEntradaProducto);
 export default router;
